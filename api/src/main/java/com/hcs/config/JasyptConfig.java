2266c34aa7dab8f12b59dcecfa6b0f17fc1fd909
noah0504789 <noah0504@naver.com>
2021-11-18 20:17:49 +0900

    using jasypt for application.yml
    application.yml with jasypt

--- api/src/main/java/com/hcs/config/JasyptConfig.java
+++ api/src/main/java/com/hcs/config/JasyptConfig.java
@@ -4,29 +4,29 @@ import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 /**
- * @ConfigurationProperties : 런타임시 프로퍼티 값을 가져오기 위해 사용됨
+ * @PropertySource : 런타임시 프로퍼티 값을 가져오기 위해 사용됨
  * @Value : properties 파일의 키-값을 주입받는데 사용됨.
  * @Bean : 개발자가 직접 제어가 불가능한 외부라이브러리 등을 Bean으로 만들려할 때 사용됨.
  * : 1개 이상의 @Bean을 제공하는 클래스의 경우 반드시 @Configuration을 명시해 주어야 함
  */
 
 @Configuration
-@ConfigurationProperties(prefix = "jasypt.encryptor")
+@PropertySource("application.yml")
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password}")
-    private String password;
+    @Value("${jasypt.encryptor.password}")// 시크릿 키
+    private String encryptKey;
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(password);
+        config.setPassword(encryptKey);
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
@@ -35,5 +35,4 @@ public class JasyptConfig {
         encryptor.setConfig(config);
         return encryptor;
     }
-
 }
