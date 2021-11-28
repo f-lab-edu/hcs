8de3dd0b45edd8cb2354f5908835d6482262a1e5
Cheonhwa Kim <trigonotis97@gmail.com>
2021-11-29 01:23:15 +0900

    delete not necessary remark

--- src/main/java/com/hcs/domain/Club.java
+++ src/main/java/com/hcs/domain/Club.java
@@ -11,7 +11,6 @@ import java.time.LocalDateTime;
 /**
  * @Data : @Getter, @Setter, @ToString, @EqualsAndHashCode, @RequireArgsConstructor 를 한번에 설정함.
  * @EqualsAndHashCode(onlyExplicitlyIncluded = true) : equals(), hashCode() 에서 사용할 필드를 명시적으로 선언해 사용하기위해 설정함.
- * @NoArgsConstructor
  * @EqualsAndHashCode.Include : 해당 애노테이션으로 지정한 필드만  equals(), hashCode() 에서 사용한다.
  */
 @Data @Builder
