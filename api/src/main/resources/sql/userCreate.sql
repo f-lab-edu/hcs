7d16c369c5efc6ad3de8bd52f8021d3fc34ac05a
Cheonhwa Kim <trigonotis97@gmail.com>
2021-12-06 15:16:03 +0900

    refactor : Separating domain Sql

--- api/src/main/resources/sql/userCreate.sql
+++ api/src/main/resources/sql/userCreate.sql
@@ -12,13 +12,3 @@ create table User
     position                   VARCHAR(10),
     location                   VARCHAR(20)
 )
-
-create table Club
-(
-    id                         int AUTO_INCREMENT PRIMARY KEY COMMENT '동호회 id key',
-    title                      VARCHAR(20) NOT NULL,
-    description                VARCHAR(50),
-    created_at                 datetime NOT NULL,
-    location                   VARCHAR(20) NOT NULL,
-    category                   VARCHAR(20) NOT NULL
-)
