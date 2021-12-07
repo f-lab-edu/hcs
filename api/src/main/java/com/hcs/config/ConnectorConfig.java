44ba115b8946bf9f4b532fd02377bf7bf1ca9317
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2021-12-08 03:15:43 +0900

    erase unnecessary annotation

--- api/src/main/java/com/hcs/config/ConnectorConfig.java
+++ api/src/main/java/com/hcs/config/ConnectorConfig.java
@@ -4,16 +4,12 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@ComponentScan
 @Configuration
-@EnableAutoConfiguration
 public class ConnectorConfig {
 
     @Bean
