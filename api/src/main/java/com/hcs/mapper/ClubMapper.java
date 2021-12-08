ece6392e1271f6f75a9792445a62a23b454f7809
Cheonhwa Kim <trigonotis97@gmail.com>
2021-12-08 20:43:34 +0900

    docs :add param annotation description

--- api/src/main/java/com/hcs/mapper/ClubMapper.java
+++ api/src/main/java/com/hcs/mapper/ClubMapper.java
@@ -4,7 +4,9 @@ import com.hcs.domain.Club;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
-import java.util.List;
+/**
+ * @Param : mybatis 에서 sql 문장에 다수의 파라미터를 전달할 때 변수이름을 각각 지정해주기위해 사용한다.
+ */
 
 @Mapper
 public interface ClubMapper {
