d4a236c1615c34ff34b1d8183b7136d230a87788
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2021-12-13 20:50:12 +0900

    addComment (#46)
    * addComment
    
    * Update CommentController.java
    
    1. import 라인 * -> 상세경로들로 작성
    2. TradePost의 Dummy데이터를 생성할 때 id값을 url에서 받아와 생성함

--- api/src/main/java/com/hcs/controller/CommentController.java
+++ api/src/main/java/com/hcs/controller/CommentController.java
@@ -1,14 +1,17 @@
 package com.hcs.controller;
 
+import com.hcs.domain.TradePost;
+import com.hcs.domain.User;
 import com.hcs.dto.CommentDto;
+import com.hcs.dto.RespCommentDto;
+import com.hcs.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
 
@@ -17,10 +20,29 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class CommentController {
 
+    private final CommentService commentService;
+//    private final UserService userService; // 충분히 구현된 후 사용될 것임
+//    private final TradePostService tradePostService;
+
+
     @PostMapping("/{id}/comment/add")
-    public void addComment(@Valid @RequestBody CommentDto commentDto, HttpServletResponse response,
-                           Model model) throws IOException {
-        // TODO TradePost글에 댓글을 남기는 작업.
-        response.sendRedirect("/post/tradePost/{id}");
+    public RespCommentDto addComment(@Valid @RequestBody CommentDto commentDto, @PathVariable("id") Long tradePostId) throws IOException {
+
+        User user = User.builder()
+                .id(31L)
+                .build(); // Dummy 데이터
+
+        TradePost tradePost = TradePost.builder() // Dummy 데이터
+                .id(tradePostId)
+                .build();
+
+        int commentId = commentService.saveNewComment(commentDto, user, tradePost);
+        boolean success = false;
+
+        if (commentId > 0) {
+            success = true;
+        }
+
+        return new RespCommentDto(commentId, success);
     }
 }
