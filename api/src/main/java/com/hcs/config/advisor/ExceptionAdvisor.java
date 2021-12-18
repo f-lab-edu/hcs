442919591f3b74a5da950ce8f51e795a640de88b
noah0504789 <noah0504@naver.com>
2021-12-19 02:44:32 +0900

    ParameterizedTest

--- api/src/main/java/com/hcs/config/advisor/ExceptionAdvisor.java
+++ api/src/main/java/com/hcs/config/advisor/ExceptionAdvisor.java
@@ -3,10 +3,12 @@ package com.hcs.config.advisor;
 import com.hcs.config.advisor.result.ValidationResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,6 +26,7 @@ public class ExceptionAdvisor {
     @Autowired
     private MessageSource messageSource;
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ValidationResult handleBindException(BindException bindException, Locale locale) {
         return ValidationResult.create(bindException, messageSource, locale);
