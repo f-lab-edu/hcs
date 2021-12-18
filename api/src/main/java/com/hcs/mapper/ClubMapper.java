21bb34a30d2c5f21367e89f803573303e1ac0566
Cheonhwa Kim <trigonotis97@gmail.com>
2021-12-19 02:19:22 +0900

    feat : adding club paging code in CubMapper

--- api/src/main/java/com/hcs/mapper/ClubMapper.java
+++ api/src/main/java/com/hcs/mapper/ClubMapper.java
@@ -3,6 +3,9 @@ package com.hcs.mapper;
 import com.hcs.domain.Club;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
 
 /**
  * @Param : mybatis 에서 sql 문장에 다수의 파라미터를 전달할 때 변수이름을 각각 지정해주기위해 사용한다.
@@ -13,6 +16,7 @@ public interface ClubMapper {
 
     Club findById(Long id);
 
+    //TODO: return type 을 List 로 변경하기
     Club findByTitle(String title);
 
     void save(Club club);
@@ -26,5 +30,13 @@ public interface ClubMapper {
     void joinMemberById(@Param("clubId") Long clubId, @Param("memberId") Long userId);
 
     void joinManagerById(@Param("clubId") Long clubId, @Param("managerId") Long userId);
+
+    List<Club> findClubWithPaging(RowBounds rowBounds);
+
+    void deleteClub(Long id);
+
+    List<Club> findAllClubs();
+
+    //TODO: sort
 
 }
--- api/src/test/java/com/hcs/mapper/ClubMapperTest.java
+++ api/src/test/java/com/hcs/mapper/ClubMapperTest.java
@@ -3,6 +3,8 @@ package com.hcs.mapper;
 import com.hcs.domain.Club;
 import com.hcs.domain.User;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -23,6 +29,8 @@ class ClubMapperTest {
     ClubMapper clubMapper;
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    SqlSession session;
 
     @DisplayName("ClubMapper - club db에 저장 및 찾기 테스트")
     @Test
@@ -74,7 +82,7 @@ class ClubMapperTest {
         Club testClub = clubMapper.findByTitle("testClub");
 
         int memberSize = 3;
-        generateAndJoinClub(testClub, "member", memberSize);
+        Set<User> userSet = generateAndJoinClub(testClub, UserType.MEMBER, memberSize);
 
         Club aClubWithMembers = clubMapper.findClubWithMembers(testClub.getId());
         assertNotNull(aClubWithMembers);
@@ -114,7 +122,7 @@ class ClubMapperTest {
         Club testClub = clubMapper.findByTitle("testClub");
 
         int managerSize = 5;
-        generateAndJoinClub(testClub, "manager", managerSize);
+        Set<User> userSet = generateAndJoinClub(testClub, UserType.MANAGER, managerSize);
 
         Club clubWithManagers = clubMapper.findClubWithManagers(testClub.getId());
         assertNotNull(clubWithManagers);
@@ -122,4 +130,64 @@ class ClubMapperTest {
 
     }
 
+    private Set<User> generateAndJoinClub(Club club, UserType userType, int userSize) {
+        Set<User> userSet = new HashSet<>();
+        for (int i = 0; i < userSize; i++) {
+            String username = "testuser" + i;
+            User user = User.builder()
+                    .email(username + "@gmail.com")
+                    .nickname(username)
+                    .password(username + "pass").build();
+
+            userMapper.insertUser(user);
+            User newUser = userMapper.findByEmail(username + "@gmail.com");
+            if (userType == UserType.MANAGER) {
+                clubMapper.joinManagerById(club.getId(), newUser.getId());
+            } else if (userType == UserType.MEMBER) {
+                clubMapper.joinMemberById(club.getId(), newUser.getId());
+            }
+            userSet.add(user);
+        }
+        return userSet;
+    }
+
+    private List<Club> generateClub(int clubSize) {
+        List<Club> clubList = new ArrayList<>();
+        for (int i = 0; i < clubSize; i++) {
+            Club club = Club.builder().title("testClub_" + i)
+                    .createdAt(LocalDateTime.now())
+                    .description("this is club for test")
+                    .location("Mars")
+                    .category("test")
+                    .build();
+            clubMapper.insertClub(club);
+            clubList.add(club);
+        }
+        return clubList;
+    }
+
+    @DisplayName("mybatis mapper pagination test")
+    @Test
+    void findAllClubWithPageTest() {
+        List<Club> clubList = generateClub(30);
+        int limit = 5, start = 0;
+        RowBounds rowBounds = new RowBounds(start, limit);
+        List<Club> pagingClubList = session.selectList("com.hcs.mapper.ClubMapper.findClubWithPaging", null, rowBounds);
+        assertEquals(pagingClubList.size(), limit);
+        assertEquals(clubList.get(start).getId(), pagingClubList.get(0).getId());
+        assertEquals(clubList.get(start + limit - 1).getId(), pagingClubList.get(limit - 1).getId()); // 페이징 마지막 값
+
+        limit = 10;
+        start = 20;
+        rowBounds = new RowBounds(start, limit);
+        pagingClubList = session.selectList("com.hcs.mapper.ClubMapper.findClubWithPaging", null, rowBounds);
+        assertEquals(pagingClubList.size(), limit);
+        assertEquals(clubList.get(start).getId(), pagingClubList.get(0).getId());
+        assertEquals(clubList.get(start + limit - 1).getId(), pagingClubList.get(limit - 1).getId());
+    }
+
+    enum UserType {
+        MANAGER,
+        MEMBER
+    }
 }
