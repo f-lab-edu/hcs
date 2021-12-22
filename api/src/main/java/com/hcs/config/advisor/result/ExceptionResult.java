edc7e5b3530c59e20ccf7bb2c172d2c104916e58
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2021-12-22 19:13:49 +0900

    ErrorCode (#63)
    * ErrorCode
    HcsException, ExceptionResult, HcsResponse
    
    * refactoring HcsResponse

--- api/src/main/java/com/hcs/config/advisor/result/ExceptionResult.java
+++ api/src/main/java/com/hcs/config/advisor/result/ExceptionResult.java
@@ -1,20 +1,12 @@
 package com.hcs.config.advisor.result;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Data;
 
-import java.sql.Timestamp;
-
-@Getter
+@Data
+@AllArgsConstructor
 public class ExceptionResult {
-    private Timestamp timestamp;
-    private String message;
-    private String reason;
 
-    public ExceptionResult( String message, String reason) {
-        this.timestamp = new Timestamp(System.currentTimeMillis());
-        this.message = message;
-        this.reason = reason;
-    }
+    private int errorCode;
+    private String message;
 }
