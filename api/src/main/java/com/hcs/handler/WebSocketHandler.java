773312a91de4d1bb2dfe131ee527d9f75be7e899
noah0504789 <noah0504@naver.com>
2021-12-27 18:19:10 +0900

    init STOMP

---
+++ api/src/main/java/com/hcs/handler/WebSocketHandler.java
@@ -0,0 +1,26 @@
+package com.hcs.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    private Set<WebSocketSession> sessions = new ConcurrentHashMap().newKeySet();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+}
