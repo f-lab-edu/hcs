215ba2c6be129f83afaff7c25f4837a78cae1e30
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2021-12-28 21:54:35 +0900

    createChatRoom (#77)

---
+++ api/src/main/java/com/hcs/dto/ChatMessageDto.java
@@ -0,0 +1,12 @@
+package com.hcs.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ChatMessageDto {
+    private String roomId;
+    private long authorId;
+    private String message;
+}
