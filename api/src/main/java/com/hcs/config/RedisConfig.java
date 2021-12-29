b5871bc24a146816d6cd7a81ec7cb55e8cea20ad
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2021-12-29 11:46:55 +0900

    setting redis (#78)
    * setting redis

---
+++ api/src/main/java/com/hcs/config/RedisConfig.java
@@ -0,0 +1,47 @@
+package com.hcs.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @EnableRedisRepositories : Redis 관련 설정을 세팅하는 파일로 Spring이 redis 사용시 적용하도록 하도록 알려주게된다
+ */
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+        return template;
+    }
+
+}
