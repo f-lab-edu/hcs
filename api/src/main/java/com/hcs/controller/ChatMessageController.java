4b188a2353bab70b2b0cd13ba30f80a975f5e881
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2022-01-01 17:05:50 +0900

    addMessage (#82)

--- api/src/main/java/com/hcs/controller/ChatMessageController.java
+++ api/src/main/java/com/hcs/controller/ChatMessageController.java
@@ -1,11 +1,19 @@
 package com.hcs.controller;
 
+import com.hcs.domain.ChatMessage;
+import com.hcs.domain.User;
 import com.hcs.dto.ChatMessageDto;
+import com.hcs.dto.response.HcsResponse;
 import com.hcs.dto.response.HcsResponseManager;
+import com.hcs.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+/**
+ * @MessageMapping : 해당 url로 요청이오면 메소드가 실행됨
+ */
 
 @Controller
 @RequiredArgsConstructor
@@ -13,19 +21,20 @@ public class ChatMessageController {
 
     private final SimpMessagingTemplate template;
     private final HcsResponseManager hcsResponseManager;
-//    private final ChatMessageService chatMessageService; // 생성한 이후에 활성화 할 것
+    private final ChatMessageService chatMessageService;
 
     @MessageMapping("/chat/message") // pub
-    public void message(ChatMessageDto message) {
+    public HcsResponse message(ChatMessageDto message) {
+
+        User curUser = User.builder()
+                .id(31L)
+                .build(); // Dummy 데이터
 
         // to subscriber
         template.convertAndSend("/sub/chat/room?roomId=" + message.getRoomId(), message);
 
-//        ChatMessage newChat  = chatMessageService.createChatMessage(message);
+        ChatMessage newChat = chatMessageService.createChatMessage(message);
 
-        // chatRoom의 chatMessages(Set자료구조)에 현재 글쓴 메시지 저장, members에 자신 추가
-        // /chat/room?roomId={roomId}로 redirect
-
-//		return hcsResponseManager.submit.ChatMessage(200, );
+        return hcsResponseManager.submit.chatMessage(newChat.getId());
     }
 }
