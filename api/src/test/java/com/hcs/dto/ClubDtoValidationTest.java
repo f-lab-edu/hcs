4282e0478407862305926beba49a7f1ab1871004
Cheonhwa Kim <trigonotis97@gmail.com>
2022-01-02 04:05:28 +0900

    clubDto.category-club.categoryId mapping 추가 및 테스트 수정

--- api/src/test/java/com/hcs/dto/ClubDtoValidationTest.java
+++ api/src/test/java/com/hcs/dto/ClubDtoValidationTest.java
@@ -1,6 +1,7 @@
 package com.hcs.dto;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hcs.config.EnableMockMvc;
 import com.hcs.domain.Club;
 import com.hcs.dto.request.ClubDto;
 import com.hcs.mapper.ClubMapper;
@@ -23,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@EnableMockMvc
 @Transactional
 class ClubDtoValidationTest {
 
@@ -49,7 +50,7 @@ class ClubDtoValidationTest {
                 .accept(MediaType.APPLICATION_JSON))
                 //.with(csrf())) // security 설정 이후 코드 사용 예정
                 .andDo(print())
-                .andExpect(status().isOk())
+                .andExpect(status().is4xxClientError())
                 .andExpect(
                         (result) -> assertTrue(result.getResolvedException().getClass()
                                 .isAssignableFrom(MethodArgumentNotValidException.class))
--- api/src/test/java/com/hcs/service/CategoryServiceTest.java
+++ api/src/test/java/com/hcs/service/CategoryServiceTest.java
@@ -5,6 +5,8 @@ import com.hcs.mapper.CategoryMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -27,8 +29,8 @@ class CategoryServiceTest {
     @DisplayName("카테고리 리스트가 주어지면, 그 리스트 반환하기")
     @Test
     void getAllCategory(){
-        List<Category> givenCategoryList = new ArrayList<>();
         //given
+        List<Category> givenCategoryList = new ArrayList<>();
         given(categoryMapper.selectAllCategory()).willReturn(givenCategoryList);
 
         //when
@@ -36,6 +38,30 @@ class CategoryServiceTest {
 
         //then
         assertEquals(categoryList, givenCategoryList);
+
+    }
+
+    @DisplayName("카테고리 이름이 주어지면, 그 이름에 해당하는 categoryId 반환하기")
+    @ParameterizedTest
+    @ValueSource(strings={"sports","reading","music"})
+    void getCategoryId(String categoryName) {
+        //given
+        List<Category> givenCategoryList = new ArrayList<>();
+        givenCategoryList.add(new Category(1L, "sports"));
+        givenCategoryList.add(new Category(2L, "reading"));
+        givenCategoryList.add(new Category(3L, "music"));
+        given(categoryMapper.selectAllCategory()).willReturn(givenCategoryList);
+
+        //when
+        Long categoryId = categoryService.getCategoryId(categoryName);
+
+        //then
+        String givenCategoryName="";
+        for (Category c: givenCategoryList) {
+            if(categoryId.equals(c.getId()))
+                givenCategoryName = c.getName();
+        }
+        assertEquals(categoryName,givenCategoryName);
 
     }
 }
