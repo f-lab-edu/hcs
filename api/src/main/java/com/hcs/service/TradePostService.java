473e22170306409269a1417828521b1ac5cbc85f
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2022-01-02 05:56:22 +0900

    tradePostDtoValidator (#88)

--- api/src/main/java/com/hcs/service/TradePostService.java
+++ api/src/main/java/com/hcs/service/TradePostService.java
@@ -1,18 +1,24 @@
 package com.hcs.service;
 
+import com.hcs.domain.TradePost;
 import com.hcs.dto.TradePostDto;
+import com.hcs.mapper.TradePostMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import javax.validation.Valid;
 
 @Service
 @RequiredArgsConstructor
 public class TradePostService {
 
-    public boolean saveTradePost(@Valid TradePostDto tradePostDto) {
+    private final TradePostMapper tradePostMapper;
+
+    public boolean saveTradePost(TradePostDto tradePostDto) {
         // TODO 중고게시글 저장하기
         return true;
+    }
+
+    public TradePost findByTitle(String title) {
+        return tradePostMapper.findByTitle(title);
     }
 
 }
--- api/src/main/resources/sql/tradePostCreate.sql
+++ api/src/main/resources/sql/tradePostCreate.sql
@@ -1,17 +1,18 @@
 create table TradePost
 (
-    id                  int AUTO_INCREMENT PRIMARY KEY COMMENT '중고거래 게시글 id key',
-    number              int          NOT NULL,
-    authorId            int          NOT NULL,
-    title               VARCHAR(30)  NOT NULL,
-    productStatus       VARCHAR(10)  NOT NULL,
-    category            VARCHAR(15)  NOT NULL,
-    description         VARCHAR(250) NOT NULL,
-    pictures            blob,
-    appointmentLocation VARCHAR(250),
-    price               int          NOT NULL,
-    salesStatus         boolean      NOT NULL,
-    registerationTime   datetime,
+    id                int AUTO_INCREMENT PRIMARY KEY COMMENT '중고거래 게시글 id key',
+    authorId          int          NOT NULL,
+    title             VARCHAR(30)  NOT NULL,
+    productStatus     VARCHAR(10)  NOT NULL,
+    category          VARCHAR(15)  NOT NULL,
+    description       VARCHAR(250) NOT NULL,
+    pictures          blob,
+    locationName      VARCHAR(100),
+    lat               DECIMAL(10, 8),
+    lng               DECIMAL(11, 8),
+    price             int          NOT NULL,
+    salesStatus       boolean      NOT NULL,
+    registerationTime datetime     NOT NULL,
 
     FOREIGN KEY (authorId) REFERENCES User (id) ON DELETE CASCADE
 )
