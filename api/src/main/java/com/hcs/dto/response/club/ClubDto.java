52eb0b4abd959221f6994d0041cdbaa2fc9bbf0d
Cheonhwa Kim <trigonotis97@gmail.com>
2022-01-12 01:46:04 +0900

    사용하지 않는 어노테이션 기능 제외

--- api/src/main/java/com/hcs/dto/response/club/ClubDto.java
+++ api/src/main/java/com/hcs/dto/response/club/ClubDto.java
@@ -1,10 +1,12 @@
 package com.hcs.dto.response.club;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 public class ClubDto {
     private Long clubId;
     private String clubUrl;
--- api/src/main/java/com/hcs/dto/response/club/ClubInListDto.java
+++ api/src/main/java/com/hcs/dto/response/club/ClubInListDto.java
@@ -1,15 +1,17 @@
 package com.hcs.dto.response.club;
 
-import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @EqualsAndHashCode(callSuper = true) : equals, hashcode 메소드사용시 슈퍼클래스 값 포함
  */
 
-@Data
+@Getter
+@Setter
 @EqualsAndHashCode(callSuper = true)
-public class ClubInListDto extends ClubDto{
+public class ClubInListDto extends ClubDto {
     private int managerCount;
     private int memberCount;
 }
