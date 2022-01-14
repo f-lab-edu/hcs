a89685735d070ad1254c3bdeae382c67c376cafb
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2022-01-15 06:08:47 +0900

    mapping columns with jpa in user, tradepost, comment domain (#130)

--- api/src/main/java/com/hcs/domain/TradePost.java
+++ api/src/main/java/com/hcs/domain/TradePost.java
@@ -1,20 +1,26 @@
 package com.hcs.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
 @Data
+@Entity
+@Table(name = "TradePost")
 @Builder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @NoArgsConstructor
@@ -22,25 +28,51 @@ import java.util.Set;
 public class TradePost {
 
     @EqualsAndHashCode.Include
-    @JsonIgnore
+    @Id
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "title")
     private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "authorId")
     private User author;
+
+    @Column(name = "productStatus")
     private String productStatus;
+
+    @Column(name = "category")
     private String category;
+
+    @Column(name = "description")
     private String description;
+
     @Lob
+    @Column(name = "pictures")
     private byte[] pictures;
+
+    @Column(name = "locationName")
     private String locationName;
+
+    @Column(name = "lng")
     private double lng;
+
+    @Column(name = "lat")
     private double lat;
+
+    @Column(name = "price")
     private int price;
+
+    @Column(name = "views")
     private int views;
 
+    @OneToMany(mappedBy = "tradePost")
     private Set<Comment> comments = new HashSet<>();
 
+    @Column(name = "salesStatus")
     private boolean salesStatus;
-    @Temporal(TemporalType.TIMESTAMP)
+
+    @Column(name = "registerationTime")
     private LocalDateTime registerationTime;
 }
