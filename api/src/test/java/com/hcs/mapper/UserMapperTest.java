d570b8fdc5c40ce41fa3d93028acfb827e3e1c20
noah0504789 <noah0504@naver.com>
2022-01-18 16:27:40 +0900

    UserMapperTest

--- api/src/test/java/com/hcs/mapper/UserMapperTest.java
+++ api/src/test/java/com/hcs/mapper/UserMapperTest.java
@@ -10,7 +10,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,12 +30,40 @@ class UserMapperTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    void insertTestUser(String newEmail, String newNickname, String newPassword) {
+    long insertTestUser(String newEmail, String newNickname, String newPassword) {
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
         String insertSql = "insert into User (email, nickname, password)\n" +
                 "values (?, ?, ?)";
 
-        jdbcTemplate.update(insertSql, new Object[]{newEmail, newNickname, newPassword});
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, newEmail);
+            ps.setString(2, newNickname);
+            ps.setString(3, newPassword);
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
+    @DisplayName("UserMapper - 사용자 Id(DB의 Prmiary Key)로 User 찾기")
+    @Test
+    void findByIdTest() {
+
+        String newEmail = "test@naver.com";
+        String newNickname = "test";
+        String newPassword = "password";
+
+        long userId = insertTestUser(newEmail, newNickname, newPassword);
+
+        Optional<User> returnedBy = Optional.ofNullable(userMapper.findById(userId));
+
+        assertThat(returnedBy).isNotEmpty();
+        assertThat(returnedBy.get().getEmail()).isEqualTo(newEmail);
+        assertThat(returnedBy.get().getNickname()).isEqualTo(newNickname);
+        assertThat(returnedBy.get().getPassword()).isEqualTo(newPassword);
     }
 
     @DisplayName("UserMapper - 이메일로 User 찾기")
@@ -50,12 +82,11 @@ class UserMapperTest {
         assertThat(returnedBy.get().getEmail()).isEqualTo(newEmail);
         assertThat(returnedBy.get().getNickname()).isEqualTo(newNickname);
         assertThat(returnedBy.get().getPassword()).isEqualTo(newPassword);
-
     }
 
-    @DisplayName("UserMapper - 사용자 Id(DB의 Prmiary Key)로 User 찾기")
+    @DisplayName("UserMapper - 닉네임으로 User 찾기")
     @Test
-    void findByIdTest() {
+    void findByNicknameTest() {
 
         String newEmail = "test@naver.com";
         String newNickname = "test";
@@ -63,13 +94,46 @@ class UserMapperTest {
 
         insertTestUser(newEmail, newNickname, newPassword);
 
-        Optional<User> insertedUser = Optional.ofNullable(userMapper.findByEmail(newEmail));
-        Optional<User> returnedBy = Optional.ofNullable(userMapper.findById(insertedUser.get().getId()));
+        Optional<User> returnedBy = Optional.ofNullable(userMapper.findByNickname(newNickname));
 
         assertThat(returnedBy).isNotEmpty();
         assertThat(returnedBy.get().getEmail()).isEqualTo(newEmail);
         assertThat(returnedBy.get().getNickname()).isEqualTo(newNickname);
         assertThat(returnedBy.get().getPassword()).isEqualTo(newPassword);
+    }
+
+    @DisplayName("UserMapper - 이메일로 User 수 찾기")
+    @Test
+    void countByEmailTest() {
+
+        String newEmail = "test@naver.com";
+        String newNickname = "test";
+        String newPassword = "password";
+
+        insertTestUser(newEmail, newNickname, newPassword);
+        insertTestUser(newEmail, newNickname + 1, newPassword + 1);
+        insertTestUser(newEmail, newNickname + 2, newPassword + 2);
+
+        int count = userMapper.countByEmail(newEmail);
+
+        assertThat(count).isEqualTo(3);
+    }
+
+    @DisplayName("UserMapper - 닉네임으로 User 수 찾기")
+    @Test
+    void countByNicknameTest() {
+
+        String newEmail = "test@naver.com";
+        String newNickname = "test";
+        String newPassword = "password";
+
+        insertTestUser(newEmail, newNickname, newPassword);
+        insertTestUser(newEmail + 1, newNickname, newPassword + 1);
+        insertTestUser(newEmail + 2, newNickname, newPassword + 2);
+
+        int count = userMapper.countByNickname(newNickname);
+
+        assertThat(count).isEqualTo(3);
     }
 
     @DisplayName("UserMapper - insert 테스트")
@@ -86,19 +150,51 @@ class UserMapperTest {
                 .password(newPassword)
                 .build();
 
-        long newUserId = userMapper.insertUser(testUser);
+        userMapper.insertUser(testUser);
 
-        assertThat(newUserId).isGreaterThan(0);
+        assertThat(testUser.getId()).isGreaterThan(0);
+    }
+
+    @DisplayName("UserMapper - update 테스트")
+    @Test
+    void updateUserTest() {
+
+        String newEmail = "test2@naver.com";
+        String newNickname = "test2";
+        String newPassword = "password";
+
+        long userId = insertTestUser(newEmail, newNickname, newPassword);
+
+        int age = 20;
+        String position = "backend";
+        String location = "Seoul";
+
+        User modifiedUser = User.builder()
+                .id(userId)
+                .email(newEmail + 1)
+                .nickname(newNickname + 1)
+                .password(newPassword + 1)
+                .age(age)
+                .position(position)
+                .location(location)
+                .build();
+
+        int isSuccess = userMapper.updateUser(modifiedUser);
+
+        assertThat(isSuccess).isGreaterThan(0);
     }
 
     @DisplayName("UserMapper - delete 테스트")
     @Test
-    void deleteUserByEmailTest() {
+    void deleteUserByIdTest() {
 
-        String existingUserEmail = "test2@naver.com";
+        String newEmail = "test@naver.com";
+        String newNickname = "test";
+        String newPassword = "password";
 
-        long deletedUserId = userMapper.deleteUserByEmail(existingUserEmail);
+        long userId = insertTestUser(newEmail, newNickname, newPassword);
+        long isSuccess = userMapper.deleteUserById(userId);
 
-        assertThat(deletedUserId).isGreaterThan(0);
+        assertThat(isSuccess).isGreaterThan(0);
     }
 }
