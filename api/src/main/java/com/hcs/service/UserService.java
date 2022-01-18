1d1458c2f9e05adeacd4a5461b15b3c3eaa52cc3
noah0504789 <noah0504@naver.com>
2022-01-19 01:04:59 +0900

    UserService 기능 생성 및 테스트 코드

--- api/src/main/java/com/hcs/service/UserService.java
+++ api/src/main/java/com/hcs/service/UserService.java
@@ -2,12 +2,12 @@ package com.hcs.service;
 
 import com.hcs.domain.User;
 import com.hcs.dto.request.SignUpDto;
+import com.hcs.dto.request.UserModifyDto;
+import com.hcs.exception.global.DatabaseException;
 import com.hcs.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
-import javax.validation.Valid;
 
 /**
  * @Service : 루트 컨테이너에 Bean 객체로 생성해주는 어노테이션. 서비스 레이어에 등록되며 자바 로직을 처리함.
@@ -17,21 +17,20 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class UserService {
 
-    //private final PasswordEncoder passwordEncoder; // security 설정 이후 코드 사용 예정
     private final ModelMapper modelMapper;
     private final UserMapper userMapper;
 
-    public User saveNewUser(@Valid SignUpDto signUpDto) {
-        //signUpDto.setPassword(passwordEncoder.encode(signUpDto.getPassword())); // security 설정 이후 코드 사용 예정
-        User user = modelMapper.map(signUpDto, User.class);
-        try {
-            insertUser(user);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-        return user;
+    public User saveNewUser(SignUpDto signUpDto) {
 
+        User user = modelMapper.map(signUpDto, User.class);
+
+        long isSuccess = insertUser(user);
+
+        if (isSuccess != 1) {
+            throw new DatabaseException("DB user insert");
+        }
+
+        return user;
     }
 
     public User findById(long userId) {
@@ -42,8 +41,32 @@ public class UserService {
         return userMapper.findByEmail(email);
     }
 
+    public int countByEmail(String email) {
+        return userMapper.countByEmail(email);
+    }
+
+    public int countByNickname(String nickname) {
+        return userMapper.countByNickname(nickname);
+    }
+
     public long insertUser(User user) {
         return userMapper.insertUser(user);
+    }
+
+    public long modifyUser(long userId, UserModifyDto userModifyDto) {
+
+        User user = findById(userId);
+
+        user = modelMapper.map(userModifyDto, User.class);
+        user.setId(userId);
+
+        int isSuccess = userMapper.updateUser(user);
+
+        if (isSuccess != 1) {
+            throw new DatabaseException("DB user modify");
+        }
+
+        return user.getId();
     }
 
     public long deleteUserById(long userId) {
