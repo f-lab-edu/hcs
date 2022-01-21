7e2adeb23cbd48e44d62e4dac318b7e5ed2a43bf
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2022-01-21 23:24:02 +0900

    CommentService 기능 생성 및 테스트 코드 (#148)

--- api/src/main/java/com/hcs/service/CommentService.java
+++ api/src/main/java/com/hcs/service/CommentService.java
@@ -4,6 +4,7 @@ import com.hcs.domain.Comment;
 import com.hcs.domain.TradePost;
 import com.hcs.domain.User;
 import com.hcs.dto.request.CommentDto;
+import com.hcs.exception.global.DatabaseException;
 import com.hcs.mapper.CommentMapper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -17,12 +18,51 @@ public class CommentService {
     private final ModelMapper modelMapper;
 
     public long saveNewComment(CommentDto commentDto, User user, TradePost tradePost) {
+
         Comment comment = modelMapper.map(commentDto, Comment.class);
         comment.setAuthor(user);
         comment.setTradePost(tradePost);
-        commentMapper.insertComment(comment);
+
+        long isSuccess = commentMapper.insertComment(comment);
+        if (isSuccess != 1) throw new DatabaseException("DB comment insert");
+
         return comment.getId();
     }
 
-    // TODO 대댓글 comment를 DB에 반영하는 함수 작성하기
+    public long saveNewReply(CommentDto commentDto, User user, TradePost tradePost, long parentCommentId) {
+
+        Comment reply = modelMapper.map(commentDto, Comment.class);
+        reply.setAuthor(user);
+        reply.setTradePost(tradePost);
+        reply.setParentCommentId(parentCommentId);
+
+        long isSuccess = commentMapper.insertReply(reply);
+        if (isSuccess != 1) throw new DatabaseException("DB reply insert");
+
+        return reply.getId();
+    }
+
+    public Comment findById(long commentId) {
+        return commentMapper.findById(commentId);
+    }
+
+    public long modifyComment(long commentId, CommentDto commentDto) {
+
+        Comment comment = findById(commentId);
+
+        String contents = commentDto.getContents();
+        comment.setContents(contents);
+
+        int isSuccess = commentMapper.updateComment(comment);
+
+        if (isSuccess != 1) {
+            throw new DatabaseException("DB user modify");
+        }
+
+        return comment.getId();
+    }
+
+    public long deleteCommentById(long commentId) {
+        return commentMapper.deleteComment(commentId);
+    }
 }
--- api/src/test/java/com/hcs/service/CommentServiceTest.java
+++ api/src/test/java/com/hcs/service/CommentServiceTest.java
@@ -1,21 +1,20 @@
 package com.hcs.service;
 
+import com.hcs.common.JdbcTemplateHelper;
 import com.hcs.domain.Comment;
 import com.hcs.domain.TradePost;
 import com.hcs.domain.User;
 import com.hcs.dto.request.CommentDto;
-import com.hcs.mapper.CommentMapper;
-import org.junit.jupiter.api.BeforeEach;
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 /**
  * @ExtendWith : 테스트 클래스가 Mockito를 사용함을 의미함.
@@ -24,57 +23,134 @@ import static org.mockito.Mockito.when;
  * @BeforeEach : 테스트 케이스 시작 전에 먼저 실행되는 어노테이션.
  */
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@EnableEncryptableProperties
+@Transactional
 class CommentServiceTest {
 
-    @Mock
-    CommentMapper commentMapper;
+    @Autowired
+    UserService userService;
 
-    @Mock
-    ModelMapper modelMapper;
+    @Autowired
+    TradePostService tradePostService;
 
-    @InjectMocks
+    @Autowired
     CommentService commentService;
 
-    // 테스트에 사용될 Dummy 데이터
-    User author;
-    TradePost tradePost;
-    CommentDto commentDto;
-
-    @BeforeEach
-    public void setUp() {
-        modelMapper = new ModelMapper();
-        commentService = new CommentService(commentMapper, modelMapper);
-
-        author = User.builder().build(); // Dummy 데이터
-        tradePost = TradePost.builder().build();
-        commentDto = CommentDto.builder().build();
-    }
+    @Autowired
+    JdbcTemplateHelper jdbcTemplateHelper;
 
     @Test
     @DisplayName("Comment 추가가 제대로 동작하는지 테스트")
-    void testSaveNewComment() {
+    void saveNewCommentTest() {
 
-        // given
-        long testAuthorId = 31L;
-        long testTradePostId = 1L;
-        long newCommentId = 1L;
+        String newEmail = "test@naver.com";
+        String newNickname = "test";
+        String newPassword = "password";
 
-        String commentContents = "테스트 댓글 입니다.";
+        long authorId = jdbcTemplateHelper.insertTestUser(newEmail, newNickname, newPassword);
 
-        author.setId(testAuthorId);
-        tradePost.setId(testTradePostId);
-        commentDto.setContents(commentContents);
+        String title = "test";
+        String productStatus = "중";
+        String category = "중";
+        String description = "중";
+        int price = 10000;
+        int salesStatus = 0;
+        LocalDateTime registrationTime = LocalDateTime.now();
 
-        Comment comment = Comment.builder()
-                .author(author)
-                .tradePost(tradePost)
+        long tradePostId = jdbcTemplateHelper.insertTestTradePost(authorId, title, productStatus, category, description, price, salesStatus, registrationTime);
+
+        String contents = "test 댓글내용";
+
+        CommentDto commentDto = CommentDto.builder()
+                .contents(contents)
                 .build();
 
-        // when
-        when(commentMapper.insertComment(comment)).thenReturn(newCommentId);
+        User author = userService.findById(authorId);
+        TradePost tradePost = tradePostService.findById(tradePostId);
 
-        // then
-        assertThat(commentService.saveNewComment(commentDto, author, tradePost)).isEqualTo(newCommentId);
+        long newCommentId = commentService.saveNewComment(commentDto, author, tradePost);
+
+        Comment comment = commentService.findById(newCommentId);
+
+        assertThat(comment.getContents()).isEqualTo(contents);
+    }
+
+    @Test
+    @DisplayName("Reply 추가가 제대로 동작하는지 테스트")
+    void saveNewReplyTest() {
+
+        String newEmail = "test@naver.com";
+        String newNickname = "test";
+        String newPassword = "password";
+
+        long authorId = jdbcTemplateHelper.insertTestUser(newEmail, newNickname, newPassword);
+
+        String title = "test";
+        String productStatus = "중";
+        String category = "중";
+        String description = "중";
+        int price = 10000;
+        int salesStatus = 0;
+        LocalDateTime registrationTime = LocalDateTime.now();
+
+        long tradePostId = jdbcTemplateHelper.insertTestTradePost(authorId, title, productStatus, category, description, price, salesStatus, registrationTime);
+
+        String contents = "test 댓글내용";
+
+        long parentCommentId = jdbcTemplateHelper.insertTestComment(0, authorId, tradePostId, contents);
+
+        String replyContents = "test 댓글내용_reply";
+
+        CommentDto replyDto = CommentDto.builder()
+                .contents(replyContents)
+                .build();
+
+        User author = userService.findById(authorId);
+        TradePost tradePost = tradePostService.findById(tradePostId);
+
+        long replyId = commentService.saveNewReply(replyDto, author, tradePost, parentCommentId);
+
+        Comment reply = commentService.findById(replyId);
+
+        assertThat(reply.getContents()).isEqualTo(replyContents);
+    }
+
+    @Test
+    @DisplayName("Comment 수정이 제대로 동작하는지 테스트")
+    void modifyCommentTest() {
+
+        String newEmail = "test@naver.com";
+        String newNickname = "test";
+        String newPassword = "password";
+
+        long authorId = jdbcTemplateHelper.insertTestUser(newEmail, newNickname, newPassword);
+
+        String title = "test";
+        String productStatus = "중";
+        String category = "중";
+        String description = "중";
+        int price = 10000;
+        int salesStatus = 0;
+        LocalDateTime registrationTime = LocalDateTime.now();
+
+        long tradePostId = jdbcTemplateHelper.insertTestTradePost(authorId, title, productStatus, category, description, price, salesStatus, registrationTime);
+
+        String contents = "test 댓글내용";
+
+        long commentId = jdbcTemplateHelper.insertTestComment(0, authorId, tradePostId, contents);
+
+        String modifiedContents = "modified_" + contents;
+
+        CommentDto commentDtoForModified = CommentDto.builder()
+                .contents(modifiedContents)
+                .build();
+
+        long modifiedCommentId = commentService.modifyComment(commentId, commentDtoForModified);
+
+        Comment modifiedComment = commentService.findById(modifiedCommentId);
+
+        assertThat(commentId).isEqualTo(modifiedCommentId);
+        assertThat(modifiedComment.getContents()).isEqualTo(modifiedContents);
     }
 }
