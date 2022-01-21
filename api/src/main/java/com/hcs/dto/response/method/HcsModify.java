afb208ef9c4be77a694d0042c233b8f080ca93bc
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2022-01-21 23:23:24 +0900

    UserController 기능 생성 및 테스트 코드 (#145)

--- api/src/main/java/com/hcs/dto/response/method/HcsModify.java
+++ api/src/main/java/com/hcs/dto/response/method/HcsModify.java
@@ -9,7 +9,18 @@ import org.springframework.stereotype.Component;
 public class HcsModify {
 
     @Autowired
-    private ObjectMapper objectMapper;
+    ObjectMapper objectMapper;
+
+    public ObjectNode user(long userId) {
+        ObjectNode hcs = objectMapper.createObjectNode();
+        ObjectNode item = objectMapper.createObjectNode();
+
+        item.put("userId", userId);
+
+        hcs.put("status", 200);
+        hcs.set("item", item);
+        return hcs;
+    }
 
     public ObjectNode club(long clubId, String clubUrl) {
         ObjectNode hcs = objectMapper.createObjectNode();
