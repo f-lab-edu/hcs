3dee0eee9d22bc56dfb1079d2df06a29ee2767ee
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2022-01-24 00:20:12 +0900

    tradePostController - info (#152)

--- api/src/main/java/com/hcs/controller/TradePostController.java
+++ api/src/main/java/com/hcs/controller/TradePostController.java
@@ -1,8 +1,15 @@
 package com.hcs.controller;
 
+import com.hcs.domain.TradePost;
+import com.hcs.domain.User;
 import com.hcs.dto.response.HcsResponse;
+import com.hcs.dto.response.method.HcsInfo;
+import com.hcs.dto.response.tradePost.TradePostInfoDto;
+import com.hcs.dto.response.user.UserInfoDto;
 import com.hcs.service.TradePostService;
+import com.hcs.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +23,26 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class TradePostController {
 
+    private final ModelMapper modelMapper;
+    private final UserService userService;
     private final TradePostService tradePostService;
+    private final HcsInfo info;
+
+    @GetMapping("/info")
+    public HcsResponse tradePost(@RequestParam("tradePostId") long tradePostId) {
+
+        long authorId = tradePostService.findAuthorIdById(tradePostId);
+
+        TradePost tradePost = tradePostService.findById(tradePostId);
+        User author = userService.findById(authorId);
+
+        TradePostInfoDto tradePostInfoDto = modelMapper.map(tradePost, TradePostInfoDto.class);
+        UserInfoDto userInfoDto = modelMapper.map(author, UserInfoDto.class);
+
+        tradePostInfoDto.setAuthor(userInfoDto);
+
+        return HcsResponse.of(info.tradePost(tradePostInfoDto));
+    }
 
     @GetMapping("/list")
     public HcsResponse tradePosts(@RequestParam("page") int page, @RequestParam("category") String category, @RequestParam("salesStatus") boolean salesStatus) {
