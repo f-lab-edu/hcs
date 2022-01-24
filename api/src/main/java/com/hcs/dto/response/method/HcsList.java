de1f92d37d9297b9c733dcad429b260b3041f15b
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2022-01-24 13:30:27 +0900

    tradePostController - list (#155)

--- api/src/main/java/com/hcs/dto/response/method/HcsList.java
+++ api/src/main/java/com/hcs/dto/response/method/HcsList.java
@@ -3,14 +3,13 @@ package com.hcs.dto.response.method;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.hcs.domain.TradePost;
 import com.hcs.dto.response.club.ClubInListDto;
+import com.hcs.dto.response.tradePost.TradePostListDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class HcsList {
@@ -18,31 +17,14 @@ public class HcsList {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private ObjectNode item(Map<String, Object> tradePostInfo, List<TradePost> tradePostList) {
-        ObjectNode item = objectMapper.createObjectNode();
-        ArrayNode tradePosts = objectMapper.createArrayNode();
+    public ObjectNode tradePost(TradePostListDto tradePostListDto) {
 
-        item.put("page", (int) tradePostInfo.get("page"));
-        item.put("count", (int) tradePostInfo.get("count"));
-        item.put("salesStatus", (boolean) tradePostInfo.get("salesStatus"));
-        item.put("category", (String) tradePostInfo.get("category"));
-
-        for (TradePost tradePost : tradePostList) {
-            ObjectNode tradePostNode = objectMapper.valueToTree(tradePost);
-            tradePosts.add(tradePostNode);
-        }
-
-        item.set("tradePosts", tradePosts);
-
-        return item;
-    }
-
-    public ObjectNode tradePost(Map<String, Object> tradePostInfo, List<TradePost> tradePostList) {
         ObjectNode hcs = objectMapper.createObjectNode();
-        ObjectNode item = item(tradePostInfo, tradePostList);
+        ObjectNode item = objectMapper.valueToTree(tradePostListDto);
 
         hcs.put("status", 200);
         hcs.set("item", item);
+
         return hcs;
     }
 
