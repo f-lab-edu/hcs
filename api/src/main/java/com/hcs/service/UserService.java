22d82125f83b29eb7258e918b4138a8384be6f0b
noah0504789 <noah0504@naver.com>
2022-03-06 01:14:05 +0900

    security: formLogin & logout

--- api/src/main/java/com/hcs/service/UserService.java
+++ api/src/main/java/com/hcs/service/UserService.java
@@ -4,17 +4,29 @@ import com.hcs.domain.User;
 import com.hcs.dto.request.SignUpDto;
 import com.hcs.dto.request.UserModifyDto;
 import com.hcs.mapper.UserMapper;
+import com.hcs.security.UserContext;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Service : 루트 컨테이너에 Bean 객체로 생성해주는 어노테이션. 서비스 레이어에 등록되며 자바 로직을 처리함.
  */
 
 @Service
+@Transactional
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final ModelMapper modelMapper;
     private final UserMapper userMapper;
@@ -22,6 +34,7 @@ public class UserService {
     public User saveNewUser(SignUpDto signUpDto) {
 
         User user = modelMapper.map(signUpDto, User.class);
+        user.setJoinedAt(LocalDateTime.now());
 
         insertUser(user);
 
@@ -34,6 +47,25 @@ public class UserService {
 
     public User findByEmail(String email) {
         return userMapper.findByEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = findByEmail(username);
+
+        if (user == null) {
+            System.out.println("email " + username);
+            System.out.println("not existed email");
+            throw new UsernameNotFoundException("해당 email의 사용자가 존재하지 않습니다.");
+        }
+
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority("ROLE_USER")); // TODO : user domain의 role field를 값으로 주입
+
+        UserContext userContext = new UserContext(user, roles);
+
+        return userContext;
     }
 
     public int countByEmail(String email) {
