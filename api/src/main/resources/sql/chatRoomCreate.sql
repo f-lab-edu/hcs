65cd45bda586dc930101e2bf07cd2ab9533e349c
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2022-03-14 03:21:03 +0900

    entity: chatMessage & chatRoom (#181)

--- api/src/main/resources/sql/chatRoomCreate.sql
+++ api/src/main/resources/sql/chatRoomCreate.sql
@@ -1,5 +1,8 @@
 create table ChatRoom
 (
-    id        VARCHAR(255) PRIMARY KEY COMMENT '채팅방 id key',
-    createdAt datetime
+    id             VARCHAR(255) PRIMARY KEY COMMENT '채팅방 id key',
+    lastChatMesgId int,
+    createdAt      datetime,
+
+    FOREIGN KEY (lastChatMesgId) REFERENCES ChatMessage (id) ON DELETE CASCADE
 )
