3f58fc99001d341fe2c35f90b8442d5f5f0e4ea7
noah0504789 <noah0504@naver.com>
2022-03-15 15:56:52 +0900

    redirect to messenger after login using cookie

--- api/src/main/java/com/hcs/controller/OAuth2Controller.java
+++ api/src/main/java/com/hcs/controller/OAuth2Controller.java
@@ -1,11 +1,17 @@
 package com.hcs.controller;
 
+import com.hcs.security.UserContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.net.URISyntaxException;
 
 @Slf4j
 @RestController
@@ -13,11 +19,19 @@ import javax.servlet.http.HttpServletResponse;
 public class OAuth2Controller {
 
     @GetMapping("/oauth2LoginSuccess")
-    public String loginComplete(HttpServletResponse httpResponse) {
+    public String oauth_authentication_success(@AuthenticationPrincipal UserContext user,
+                                               HttpServletRequest request,
+                                               HttpServletResponse response,
+                                               HttpSession session) throws URISyntaxException {
 
+        log.info("user : " + user.getUser());
+        log.info("session : " + session.getId());
 
-        httpResponse.setHeader("Location", "http://localhost:3090");
-        httpResponse.setStatus(302);
+        Cookie loginCookie = new Cookie("email", user.getUser().getEmail());
+
+        response.addCookie(loginCookie);
+        response.setHeader("Location", "http://localhost:3090/messenger");
+        response.setStatus(302);
 
         return "oauth2 login success!";
     }
